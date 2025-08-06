a86e6de0bc7dbdad1357dbe7029eba0fd26074f5
Anjalee-jay <anjaleejayathissa@gmail.com>
2025-08-06 12:52:14 +0530

    Grading system project

---
+++ example1.py
@@ -0,0 +1,20 @@
+print("Hello Anjalee")
+print("Welcome to Python")
+
+i=20
+if(i<15):
+    print("i is smaller than 15")
+    print("i'm in if Block")
+else:
+    print("i is not present")    
+    
+i=1 
+while(i<6):
+    print(i) 
+    i+=1
+    
+marks01= int(input("Enter your marks One"))
+marks02= int(input("Enter your marks Two")) 
+total = int(marks01)+ int(marks02)
+print("Total marks are", total)  
+     
